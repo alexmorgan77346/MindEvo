6f837e0cdad4d78c675078c97b2730d7544945bb
alexmorgan77346 <anshuraj77346@gmail.com>
2026-02-23 12:13:14 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE = 'mindevo-v1';
+const CACHE = 'mindevo-v2';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
+  './',
+  './index.html',
+  './manifest.json',
    './icon-192.png',
   './icon-512.png',
   'https://fonts.googleapis.com/css2?family=Syne:wght@400;600;700;800&family=DM+Sans:ital,wght@0,300;0,400;0,500;1,300&display=swap'
